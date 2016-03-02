70764586db5a7aa386b09fcf2942d93ca8eaa88a
Grant Timmerman <granttimmerman@gmail.com>
2016-03-02 00:41:32 -0800

    Add music player functionality

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+  // Always be scrolled to the top on page load
   setTimeout(function() {
     $(this).scrollTop(0);
   }, 0);
+
+  // Setup event handlers
+  $('.generated-music .song:not(.header)').click(function() {
+    var songName = $(this).data('name');
+    console.log('toggle: ', songName);
+    $(this).toggleClass('playing');
+  });
 });
