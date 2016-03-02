e960907c975ca3aedfd512115ff977abe48ffbe5
Grant Timmerman <granttimmerman@gmail.com>
2016-03-02 02:20:02 -0800

    Reformat code to meet PEP style guide standard

--- www/index.py
+++ www/index.py
@@ -1,18 +1,17 @@
+import algorythm_datamodel
 import os
-from flask import Flask
+from flask import Flask, request, redirect
+from flask import jsonify
 from flask import render_template
-from flask import Flask, request, redirect, url_for
 from flask import send_from_directory
-from werkzeug import secure_filename
 from os import listdir
 from os.path import isfile, join
-
-import algorythm_datamodel
 
 datamodel = algorythm_datamodel.AlgorythmDatamodel()
 
 UPLOAD_FOLDER = datamodel.UPLOAD_DIR
 CONFIG_FOLDER = datamodel.CONFIG_DIR
+GENERATED_SONG_FOLDER = datamodel.GENERATED_SONG_DIR
 ALLOWED_EXTENSIONS = set(['xml'])
 
 # Setup flask
@@ -24,15 +23,20 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 if not os.path.exists(CONFIG_FOLDER):
     os.makedirs(CONFIG_FOLDER)
+if not os.path.exists(GENERATED_SONG_FOLDER):
+    os.makedirs(GENERATED_SONG_FOLDER)
+
 
 # Helper methods
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+
 def get_uploaded_files():
     path = UPLOAD_FOLDER
     files = [{'name': f, 'url': UPLOAD_FOLDER + '/' + f} for f in listdir(path) if isfile(join(path, f))]
     return files
+
 
 # Setup routes
 
@@ -43,49 +47,52 @@ def home():
     # By default loads the animation
     animate = request.args.get('animate') != 'false'
 
-    return render_template('index.html',
+    return render_template(
+        'index.html',
         animate=animate,
         files=datamodel.getTrainingFiles(),
         trainingprocesses=
         # datamodel.getTrainingProcessNames(),
         [{
-          'name': 'file 3',
-          'progress': 50,
+            'name': 'file 3',
+            'progress': 50,
         }, {
-          'name': 'file 4',
-          'progress': 10,
+            'name': 'file 4',
+            'progress': 10,
         }],
         trainedconfigs=
         datamodel.getCompletedTrainedConfigs(),
         generationprocesses=[
-        {
-          'name': 'file 3',
-          'progress': 50,
-        }, {
-          'name': 'file 4',
-          'progress': 10,
-        }
+            {
+                'name': 'file 3',
+                'progress': 50,
+            }, {
+                'name': 'file 4',
+                'progress': 10,
+            }
         ],
         generatedmusic=
         [{
-          'name': 'file name 1',
-          'tempo': 128,
-          'length': '3:02',
+            'name': 'file name 1',
+            'tempo': 128,
+            'length': '3:02',
         }, {
-          'name': 'file name 1',
-          'tempo': 128,
-          'length': '3:02',
+            'name': 'file name 1',
+            'tempo': 128,
+            'length': '3:02',
         }, {
-          'name': 'file name 1',
-          'tempo': 128,
-          'length': '3:02',
+            'name': 'file name 1',
+            'tempo': 128,
+            'length': '3:02',
         }]
     )
+
 
 # About
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 # Upload new file
 @app.route('/upload', methods=['POST'])
@@ -97,6 +104,7 @@ def upload_file():
     else:
         raise RuntimeError("upload bad file type")
     return redirect('/?animate=false')
+
 
 # Upload new file
 @app.route('/train', methods=['POST'])
@@ -120,18 +128,20 @@ def train():
     )
     return redirect('/')
 
+
 # Generate music
 @app.route('/generate', methods=['POST'])
 def generate_music():
-  # error handling....
-  # datamodel.startMusicProcess(processName, configName, seconds, 'backend/trained_music/' + configName)
-  return redirect('/')
+    # error handling....
+    # datamodel.startMusicProcess(processName, configName, seconds, 'backend/trained_music/' + configName)
+    return redirect('/')
 
 
 # View uploaded file
 @app.route('/uploads/<name>', methods=['GET', 'POST'])
 def view_upload(name=None):
     return send_from_directory(UPLOAD_FOLDER, name)
+
 
 # View training process log
 @app.route('/trainingprocesses/<name>', methods=['GET', 'POST'])
@@ -140,7 +150,11 @@ def view_training_process(name=None):
     postfix = "</pre></body></html>"
     return prefix + datamodel.getOutputForTrainingProcess(name) + postfix
 
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify(**datamodel.getStatus())
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
-
